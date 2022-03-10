968c76ba7835a819dad3f38f0958a6fbb98975eb
Heitor Furtado <heitormf@live.com>
2022-03-10 02:46:31 -0300

    project created

---
+++ src/test/java/com/hmfurtado/csv/BatchCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmfurtado.csv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
